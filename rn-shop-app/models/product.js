8146da342851fe1d8246374f83edb9a58fc58287
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-15 11:49:39 -0700

    Add Product model and dummy data

---
+++ rn-shop-app/models/product.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(id, owner, title, imageUrl, description, price) {
+        this.id = id;
+        this.ownerId = owner;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+}
+
+export default Product;
