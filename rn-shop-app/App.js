2a0ae3b722921a08b0ed85008ab4f3ca1a0b1610
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-21 11:55:37 -0700

    Display ShopNavigator with products from state

--- rn-shop-app/App.js
+++ rn-shop-app/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer
@@ -14,9 +15,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
