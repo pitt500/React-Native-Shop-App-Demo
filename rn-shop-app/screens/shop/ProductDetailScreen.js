a9874b163538d8ee61ba74fb9d21f619e42fc5f2
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-22 10:17:32 -0700

    Configure custom fonts

--- rn-shop-app/screens/shop/ProductDetailScreen.js
+++ rn-shop-app/screens/shop/ProductDetailScreen.js
@@ -33,12 +33,14 @@ const styles = StyleSheet.create({
         height: 300
     },
     price: {
+        fontFamily: 'open-sans-bold',
         fontSize: 20,
         color: '#888',
         textAlign: 'center',
         marginVertical: 20
     },
     description: {
+        fontFamily: 'open-sans',
         fontSize: 14,
         textAlign: 'center',
         marginHorizontal: 20,
