2a6c1477b4314bfbb2ac151159a822a3a359fe68
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-26 11:21:16 -0700

    Configure remove button and actions to delete items from cart

--- rn-shop-app/store/reducers/cart.js
+++ rn-shop-app/store/reducers/cart.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../actions/cart";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cart-item";
 
 const initialState = {
@@ -31,6 +31,31 @@ export default (state = initialState, action) => {
                 ...state,
                 items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
                 totalAmount: state.totalAmount + productPrice
+            }
+
+        case REMOVE_FROM_CART:
+            const selectedCartItem = state.items[action.productId]
+            const currentQty = selectedCartItem.quantity;
+            let updatedCartItems;
+
+            if (currentQty > 1) {
+                const updatedCartItem = new CartItem(
+                    selectedCartItem.quantity - 1,
+                    selectedCartItem.productPrice,
+                    selectedCartItem.productTitle,
+                    selectedCartItem.sum - selectedCartItem.productPrice
+                );
+                updatedCartItems = {...state.items, [action.productId]: updatedCartItem};
+            } else {
+                updatedCartItems = { ...state.items };
+                delete updatedCartItems[action.productId];
+            }
+
+            const total = state.totalAmount - selectedCartItem.productPrice;
+            return {
+                ...state,
+                items: updatedCartItems,
+                totalAmount: (total >= 0) ? total : 0
             }
     }
     return state;
